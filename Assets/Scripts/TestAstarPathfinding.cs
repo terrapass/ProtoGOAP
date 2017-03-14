1f9b6db0ed6d96a9ca357059025f405572648f4c
Taras Denysenko <terrapass93@gmail.com>
2017-03-14 19:48:36 +0200

    Committed MonoBehaviours, testing A* pathfinding and forward planning. Added instances of both MonoBehaviours to the world0 scene.

---
+++ Assets/Scripts/TestAstarPathfinding.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProtoGOAP.Graphs;
+
+public class TestAstarPathfinding : MonoBehaviour
+{
+	private class Node : IGraphNode<Node>
+	{
+		private string name;
+		private IList<IGraphEdge<Node>> outgoingEdges;
+
+		public Node(string name)
+		{
+			this.name = name;
+			this.outgoingEdges = new List<IGraphEdge<Node>>();
+		}
+
+		#region IGraphNode implementation
+
+		public IEnumerable<IGraphEdge<Node>> OutgoingEdges
+		{
+			get {
+				return this.outgoingEdges;
+			}
+		}
+
+		#endregion
+
+		public override string ToString()
+		{
+			return this.name;
+		}
+
+		public void AddOutgoingEdge(IGraphEdge<Node> outgoingEdge)
+		{
+			if(!outgoingEdge.SourceNode.Equals(this))
+			{
+				throw new ArgumentException("outgoingEdge must have this Node as its source", "outgoingEdge");
+			}
+			this.outgoingEdges.Add(outgoingEdge);
+		}
+
+		public override bool Equals(object obj)
+		{
+			Node otherNode = obj as Node;
+			return otherNode != null
+				? this.name.Equals(otherNode.name)
+				: false;
+		}
+
+		public override int GetHashCode()
+		{
+			return this.name.GetHashCode();
+		}
+
+		public string Name
+		{
+			get {
+				return this.name;
+			}
+		}
+	}
+
+	private class Edge : IGraphEdge<Node>
+	{
+		#region IGraphEdge implementation
+
+		public double Cost {get;}
+
+		public Node SourceNode {get;}
+
+		public Node TargetNode {get;}
+
+		#endregion
+
+		public Edge(Node sourceNode, Node targetNode, double cost)
+		{
+			this.SourceNode = sourceNode;
+			this.TargetNode = targetNode;
+			this.Cost = cost;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("[Edge: Cost={0}, SourceNode={1}, TargetNode={2}]", Cost, SourceNode, TargetNode);
+		}
+	}
+
+	private static double Heuristic(Node sourceNode, Node targetNode)
+	{
+		if(!targetNode.Name.Equals("F"))
+		{
+			throw new NotImplementedException(
+				string.Format(
+					"Heuristic is not implemented for any target node, except for F (got {0})",
+					targetNode.Name
+				)
+			);
+		}
+
+		switch(sourceNode.Name)
+		{
+		default:
+			throw new ArgumentException(string.Format("Unknown source node {0}", sourceNode.Name), "sourceNode");
+		case "A":
+			return 4;
+		case "B":
+			return 3;
+		case "C":
+			return 3;
+		case "D":
+			return 2;
+		case "E":
+			return 2;
+		case "F":
+			return 0;
+		}
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		Node a = new Node("A");
+		Node b = new Node("B");
+		Node c = new Node("C");
+		Node d = new Node("D");
+		Node e = new Node("E");
+		Node f = new Node("F");
+
+		a.AddOutgoingEdge(new Edge(a, b, 4));
+		a.AddOutgoingEdge(new Edge(a, c, 2));
+		a.AddOutgoingEdge(new Edge(a, f, 50));
+		b.AddOutgoingEdge(new Edge(b, c, 5));
+		b.AddOutgoingEdge(new Edge(b, d, 10));
+		c.AddOutgoingEdge(new Edge(c, e, 3));
+		e.AddOutgoingEdge(new Edge(e, d, 4));
+		d.AddOutgoingEdge(new Edge(d, f, 11));
+
+		var pathfinder = new AstarPathfinder<Node>(Heuristic);
+		DateTime startTime = DateTime.Now;
+		var path = pathfinder.FindPath(a, f);
+		print(string.Format("In {1} seconds found the following path with cost {0} from A to F:", path.Cost, (DateTime.Now - startTime).TotalSeconds));
+		foreach(var edge in path.Edges)
+		{
+			print(edge);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/TestForwardPlanning.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProtoGOAP.Planning;
+using ProtoGOAP.Planning.Preconditions;
+using ProtoGOAP.Planning.Effects;
+using Terrapass.Time;
+
+public class TestForwardPlanning : MonoBehaviour
+{
+	void Start()
+	{
+		var currentWorldState = new WorldState.Builder()
+			.SetSymbol(new SymbolId("Wood"), 10)
+			.SetSymbol(new SymbolId("Stone"), 8)
+			.SetSymbol(new SymbolId("Iron"), 0)
+			.SetSymbol(new SymbolId("HouseBuilt"), 0)
+			.SetSymbol(new SymbolId("WoodInStorage"), 15)
+			.SetSymbol(new SymbolId("StoneInStorage"), 0)
+			.SetSymbol(new SymbolId("IronInStorage"), 0)
+			.SetSymbol(new SymbolId("HasAxe"), 0)
+			.SetSymbol(new SymbolId("AxesAvailable"), 1)
+			.Build();
+
+		var availableActions = new List<PlanningAction>() {
+			new PlanningAction(
+				"BuildHouse",
+				new List<IPrecondition>() {
+					new IsNotSmaller(new SymbolId("Wood"), 20),
+					new IsNotSmaller(new SymbolId("Stone"), 5)
+				},
+				new List<IEffect>() {
+					new SetTrue(new SymbolId("HouseBuilt")),
+					new Subtract(new SymbolId("Wood"), 20),
+					new Subtract(new SymbolId("Stone"), 5)
+				},
+				30
+			),
+			new PlanningAction(
+				"GetWoodFromStorage",
+				new List<IPrecondition>() {
+					new IsNotSmaller(new SymbolId("WoodInStorage"), 1)
+				},
+				new List<IEffect>() {
+					new Add(new SymbolId("Wood"), 1),
+					new Subtract(new SymbolId("WoodInStorage"), 1),
+				},
+				2
+			),
+			new PlanningAction(
+				"GetStoneFromStorage",
+				new List<IPrecondition>() {
+					new IsNotSmaller(new SymbolId("StoneInStorage"), 1)
+				},
+				new List<IEffect>() {
+					new Add(new SymbolId("Stone"), 1),
+					new Subtract(new SymbolId("StoneInStorage"), 1),
+				},
+				3
+			),
+			new PlanningAction(
+				"GetIronFromStorage",
+				new List<IPrecondition>() {
+					new IsNotSmaller(new SymbolId("IronInStorage"), 1)
+				},
+				new List<IEffect>() {
+					new Add(new SymbolId("Iron"), 1),
+					new Subtract(new SymbolId("IronInStorage"), 1),
+				},
+				2
+			),
+			new PlanningAction(
+				"CutTrees",
+				new List<IPrecondition>() {
+					new IsTrue(new SymbolId("HasAxe"))
+				},
+				new List<IEffect>() {
+					new Add(new SymbolId("Wood"), 8)
+				},
+				5
+			),
+			new PlanningAction(
+				"TakeAxe",
+				new List<IPrecondition>() {
+					new IsFalse(new SymbolId("HasAxe")),
+					new IsNotSmaller(new SymbolId("AxesAvailable"), 1)
+				},
+				new List<IEffect>() {
+					new SetTrue(new SymbolId("HasAxe")),
+					new Subtract(new SymbolId("AxesAvailable"), 1)
+				},
+				1
+			),
+			new PlanningAction(
+				"MakeAxe",
+				new List<IPrecondition>() {
+					new IsFalse(new SymbolId("HasAxe")),
+					new IsNotSmaller(new SymbolId("Wood"), 2),
+					new IsNotSmaller(new SymbolId("Iron"), 3)
+				},
+				new List<IEffect>() {
+					new SetTrue(new SymbolId("HasAxe")),
+					new Subtract(new SymbolId("Wood"), 2),
+					new Subtract(new SymbolId("Iron"), 3)
+				},
+				15
+			)
+		};
+
+		var buildHouseGoal = new Goal(
+			"BuildHouse",
+			new List<IPrecondition>() {
+				new IsTrue(new SymbolId("HouseBuilt"))
+			}
+       	);
+
+		var planner = new ForwardPlanner(8);
+
+		try
+		{
+			DateTime startTime = DateTime.Now;
+			var plan = planner.FormulatePlan(currentWorldState, availableActions, buildHouseGoal);
+			print(
+				string.Format(
+					"{0} has found a plan of length {1} to satisfy \"{2}\" in {3} seconds",
+					planner.GetType(), 
+					plan.ActionNames.Count(),
+					buildHouseGoal.Name,
+					(DateTime.Now - startTime).TotalSeconds
+				)
+			);
+			print(plan.ToString());
+		}
+		catch(PlanNotFoundException e)
+		{
+			print(e.Message);
+		}
+	}
+}
+
