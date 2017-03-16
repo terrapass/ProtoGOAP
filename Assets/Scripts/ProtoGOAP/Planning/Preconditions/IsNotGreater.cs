6b55e16da833b8f45c723ca6f7a7116c5c9b0604
Taras Denysenko <terrapass93@gmail.com>
2017-03-16 03:19:06 +0200

    Fixed an issue where distance was calculated incorrectly for IsSmaller and IsGreater precondition instances.

--- Assets/Scripts/ProtoGOAP/Planning/Preconditions/IsNotGreater.cs
+++ Assets/Scripts/ProtoGOAP/Planning/Preconditions/IsNotGreater.cs
@@ -5,7 +5,7 @@ namespace ProtoGOAP.Planning.Preconditions
 	public sealed class IsNotGreater : SingleSymbolPrecondition
 	{
 		public IsNotGreater(SymbolId symbolId, int targetValue)
-			: base(symbolId, (value) => value <= targetValue, (value) => Math.Max(0, targetValue - value))
+			: base(symbolId, (value) => value <= targetValue, (value) => Math.Max(0, value - targetValue))
 		{
 			
 		}
--- Assets/Scripts/ProtoGOAP/Planning/Preconditions/IsNotSmaller.cs
+++ Assets/Scripts/ProtoGOAP/Planning/Preconditions/IsNotSmaller.cs
@@ -7,7 +7,7 @@ namespace ProtoGOAP.Planning.Preconditions
 	public sealed class IsNotSmaller : SingleSymbolPrecondition
 	{
 		public IsNotSmaller(SymbolId symbolId, int targetValue)
-			: base(symbolId, (value) => value >= targetValue, (value) => Math.Max(0, value - targetValue))
+			: base(symbolId, (value) => value >= targetValue, (value) => Math.Max(0, targetValue - value))
 		{
 		}
 	}
